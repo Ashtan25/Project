68371e1fa15985cf5a454992c9e135c531fa9cc4
Ashish <aashishsingh1995c@gmail.com>
2019-02-24 23:27:16 +0530

    Third assignment added

--- IOS/Assignment_iOS_Day3/iOS3.playground/Contents.swift
+++ IOS/Assignment_iOS_Day3/iOS3.playground/Contents.swift
@@ -274,19 +274,19 @@ subscript (<parameters>) -> <return type> {
 2. **Create a simple subscript that outputs true if a string contains a substring and false otherwise.**
 
  */
-class substringTest {
-        subscript(index: Int) -> String {
-            let string = "Hello Agnosticdev, I love Tutorials"
-            let substring = "Agnosticdev"
-            
-            if string.contains(substring) {
-                print("I found: \(substring)")
-                return substring
+class daysofaweek {
+    private var days = ["Sunday", "Monday", "Tuesday", "Wednesday",
+                        "Thursday", "Friday", "saturday"]
+    subscript(index: Int) -> Bool {
+        get {
+            if days[index].contains("Sunday"){
+                return true
             }
             else{
-            return "nothing"
-                
+                return false
             }
-            
+        }
+    }
 }
-}
+var p = daysofaweek()
+print(p[0])
